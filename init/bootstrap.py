c64bdd8910995e8ab7cc282d7a5ed112c183b867
Mike C <micabrer@cisco.com>
2014-09-07 14:59:22 -0400

    First commit to reprository. Initial work on bootstrap python script done

---
+++ init/bootstrap.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2.7
+
+"""
+node314 Bootstrap and Remote Maintenance Script
+Version 0.1
+Author: Mike C.
+"""
+
+import subprocess
+
+
+
+
+linkUp = subprocess.call("ip link set wlan0 up")
+
+
+if (linkUp == 0)
+	print "Wlan0 is up!"
+
+
+
+
+
+
+
+
+
+print status
