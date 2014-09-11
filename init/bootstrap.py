0c838833abba6628037f7bdfa55b2a33bf407e28
Mike C <micabrer@codeHut.micabrer.me>
2014-09-10 23:16:30 -0400

    Moved bootstrap definitions to new file
    Cleaned up main script

--- init/bootstrap.py
+++ init/bootstrap.py
@@ -2,50 +2,26 @@
 
 """
 node314 Bootstrap and Remote Maintenance Script
-Version 0.1
+Version 0.2
 Author: Mike C.
 """
 
 import subprocess
 import sys
-
-def checkWireless(essids):
-
-	print >>sys.stdout, "Starting wireless search..."
-	for ap in essids:
-
-		print >>sys.stdout, "Scanning for " + ap + "..."
-		
-		try:
-
-			retcode = subprocess.call("iwlist wlan0 scan | grep -i " + ap, shell=True)
-			if retcode != 0:
-				print >>sys.stdout, ap + " not in range."
-				ap = None
-			elif retcode == 0:
-				print >>sys.stdout, ap + " in range. Attempting connection..."
-				return ap
-		except OSError as e:
-			
-			print >>sys.stdout, "Scan failed. Reason: " + e
-
-
-	return ap
-
-def associateWireless(activeAP):
-
-	#print >>sys.stdout, "Starting WPA_Supplicant for " + 
-	
-	
-
-
+from bootstrapDef.py import checkWireless, associateWireless
 
 
 essids = ['1blizzard', '1Scratch_N', '1scratch_G', 'test2']
 
-activeAP=checkWireless(essids)
 
-if activeAP == None:
-	print >>sys.stdout, "No APs in range. Closing."
-else:
-	print >>sys.stdout, "Active AP is: " + activeAP
+for ssid in essids:
+
+	inRange = checkWireless(ssid)
+	
+
+	if inRange == 0
+		print >>sys.stdout, "Associate AP would run at this point"
+	elif inRange == 1
+		print >>sys.stdout, "Would skip to next AP, previous one not in range."
+	elif inRange == 2
+		print >>sys.stdout, "Unexpected error at this point. Unclear how to handle."
