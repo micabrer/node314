b94412402cc8b6a0b3da819258db1ed68d5aaeec
Mike C <micabrer@codeHut.micabrer.me>
2014-09-11 01:49:30 -0400

    Quick test.

--- init/bootstrapDef.py
+++ init/bootstrapDef.py
@@ -5,31 +5,35 @@
 
 def checkWireless(ssid):
 
-"""
-Check for access point in range
-Return 0 if success
-Return 1 if not in range
-Return 2 if other error
-"""
+	"""
+	Check for access point in range
+	Return 0 if success
+	Return 1 if not in range
+	Return 2 if other error
+	"""
 	print >>sys.stdout, "Scanning for " + ssid + "..."
-		
-	try:
+	
+	args=['iwlist', 'wlan0', 'scan', '|', 'grep ']
+	process = subprocess.Popen(args[0], args[1], args[2], args[3], args[4], ssid)
+	if retcode != 0:
+		print >>sys.stdout, ssid + " not in range."
+		print >> sys.stdout process.pid
+		return 1
+	elif retcode == 0:
+		print >>sys.stdout, ssid + " in range."
+		return 0
 
-		retcode = subprocess.call("iwlist wlan0 scan | grep -i " + ssid, shell=True)
-		if retcode != 0:
-			print >>sys.stdout, ssid + " not in range."
-			return 1
-		elif retcode == 0:
-			print >>sys.stdout, ssid + " in range."
-			return 0
-
-		except OSError as e:
-			print >>sys.stdout, "Scan failed. Reason: " + e		
-			return 2
 	return 2
 
 
 def associateWireless(ssid):
 
-	print >>sys.stdout, "Attempting association with " + activessid
+	print >>sys.stdout, "Attempting association with " + ssid
+
+	retcode = subprocess.call("wpa_supplicant -d wext -B -c /etc/node314/wpa_supplicant/" + ssid, shell=True)
+	if retcode !=0:
+		print >>sys.stdout, "Failed to associate with " + ssid + "."
+		return 1
+	elif retcode == 0:
+		print >>sys.stdout, "Associated with " + ssid + "."
 
